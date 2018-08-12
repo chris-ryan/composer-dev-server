7f678479185fb06a8764c6f6084b2bd0864ac0d9
Chris Ryan <chris@ryanweb.id.au>
2018-08-12 22:59:31 +1000

    init commit

---
+++ serve.js
@@ -0,0 +1,16 @@
+const nodemon = require('nodemon');
+
+nodemon({
+    script: 'app.js',
+    watch: [],
+    ext: 'js json'
+  });
+  
+  nodemon.on('start', function () {
+    console.log('App has started');
+  }).on('quit', function () {
+    console.log('App has quit');
+    process.exit();
+  }).on('restart', function (files) {
+    console.log('App restarted due to: ', files);
+  });
