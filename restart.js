b9c2c76bb02981326666299510e2647a1f5c2e34
Chris Ryan <chris.ryan@ctc.usyd.edu.au>
2018-08-14 18:33:12 +1000

    added restart functions

---
+++ restart.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const semver = require('semver')
+const Network = require('./lib/network-actions');
+
+// const baseDir = process.cwd();
+const baseDir = '/Users/chris/Documents/Development/DREME'
+// get the package.json info
+let pckg = JSON.parse(fs.readFileSync(`${baseDir}/package.json`));
+const networkName = pckg.name;
+
+// Deploy the BNA (using variable bna file name)
+// composer network install --card PeerAdmin@hlfv1 --archiveFile tutorial-network@0.0.1.bna
+
+// Increment the version in package.json
+let newVer = semver.inc(pckg.version, 'patch');
+console.log('Updating network version in package.json:');
+console.log(`  ${pckg.version} -> ${newVer}`);
+pckg.version = newVer;
+fs.writeFileSync(`${baseDir}/package.json`, JSON.stringify(pckg, null, 2));
+
+//async function restartComposer() {
+   
+//}
+// Upgrade the business network
+Network.upgrade(networkName, newVer);
+
+
+// process.on('exit', function () {
+//   gracefulShutdown(function () {
+//     console.log('app being shutdown');
+//     process.kill(process.pid, 'SIGUSR2');
+//   });
+// });
+
+// ArchiveCreate.handler(options);
--- serve.js
+++ serve.js
@@ -1,27 +1,53 @@
+// const fs = require('fs');
 const nodemon = require('nodemon');
+const Archive = require('./lib/archive-actions');
 
 //set baseDir to where the app is called
-const baseDir = process.cwd();
+// const baseDir = process.cwd();
+const baseDir = '/Users/chris/Documents/Development/DREME';
 
+// get the business network name and version from package.json
+// let pckg = JSON.parse(fs.readFileSync(`${baseDir}/package.json`));
+let pckg = require(`${baseDir}/package.json`);
+//const networkName = pckg.name;
+//let version = pckg.version;
+
+// let pckg = require(`${baseDir}/package-copy.json`);
+// get the business network name from package.json
+// const networkName = pckg.name;
+
+// function handleErr(err) {
+//   console.log(err.message);
+// }
+
+// On script start...
+// Generate the business network archive and install
+Archive.installBna(pckg.name, pckg.version);
+
+
+// execute nodemon and supply options
 nodemon({
-    script: 'start.js',
-    watch: [`${baseDir}/lib`, `${baseDir}/models`],
-    ext: 'js json',
-  });
+  script: 'restart.js',
+  //watch: [`${baseDir}/lib`, `${baseDir}/models`],
+  watch: [`${baseDir}/models`],
+  runOnChangeOnly: true,
+  spawn: true, //required for runOnChangeOnly
+  ext: 'cto js json',
+});
 
-  // nodemon('-e "js json" app.js');
+  // nodemon('-e --on-change-only -w models "js json" start.js');
   
-  nodemon.on('start', function () {
-    console.log(`baseDir: ${baseDir}`);
-    console.log('App has started');
-  })
-  .on('restart', function() {
-    console.log('watch has triggered');
-  }).on('quit', function () {
-    console.log('App has quit');
-    process.exit();
-  }).on('restart', function (files) {
-    console.log('App restarted due to: ', files);
-  });
+  // nodemon.on('start', function () {
+  //   console.log(`baseDir: ${baseDir}`);
+  //   console.log('App has started');
+  // })
+  // .on('restart', function() {
+  //   console.log('watch has triggered');
+  // }).on('quit', function () {
+  //   console.log('App has quit');
+  //   process.exit();
+  // }).on('restart', function (files) {
+  //   console.log('App restarted due to: ', files);
+  // });
 
   console.log(`${baseDir}/lib`);
